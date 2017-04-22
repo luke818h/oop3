ff30b4960d1d8c63138902ca7e59422c7d9ef391
Luke Henwood <luke8h@gmail.com>
2017-04-22 13:58:53 -0500

    created instance and new objects

---
+++ main.js
@@ -0,0 +1,18 @@
+var Car = function() {
+  this.wheels = 4;
+  this.engines = 1;
+  this.seats = 5;
+};
+console.log(Car);
+// Only change code below this line.
+
+var myCar = new Car();
+ console.log(myCar);
+
+ myCar.turboType = "twin";
+
+ console.log(myCar);
+
+ myCar.nickName = "Speedy";
+
+ console.log(myCar);
